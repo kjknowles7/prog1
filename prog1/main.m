5e0cd47d82dc8010e3fb6368c715c2c6e3593146
Kevin Knowles <kjknowles@kevins-macbook-air.local>
2016-08-15 11:50:25 -0600

    Removed a println.

--- prog1/main.m
+++ prog1/main.m
@@ -14,7 +14,6 @@ int main(int argc, const char * argv[]) {
         NSLog(@"Hello, World!");
         NSLog(@"Coding is fun!");
         NSLog(@"Now committing to GitHub!");
-        NSLog(@"Now seeing if I can auto push to GitHub.");
     }
     return 0;
 }
