83ffb10854b755f5e1bd01f209863a58edee5dc4
Kevin Knowles <kjknowles@kevins-macbook-air.local>
2016-08-15 18:59:08 -0600

    Updated sample program for Fraction example.

--- prog1/main.m
+++ prog1/main.m
@@ -6,14 +6,67 @@
 //  Copyright © 2016 Kevin Knowles. All rights reserved.
 //
 
+
 #import <Foundation/Foundation.h>
+
+// Interface section
+@interface Fraction: NSObject
+
+-(void)     print;
+-(void)     setNumerator: (int) n;
+-(void)     setDenominator: (int) d;
+-(int)      getNumerator;
+-(int)      getDenominator;
+
+@end
+
+//Implementation section
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int)d
+{
+    denominator = d;
+}
+
+-(int) getNumerator
+{
+    return numerator;
+}
+
+-(int) getDenominator
+{
+    return denominator;
+}
+
+@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSLog(@"Coding is fun!");
-        NSLog(@"Now committing to GitHub!");
+        
+        //Instantiate a fraction object
+        Fraction *myFraction = [[Fraction alloc] init];
+        
+        //Set instance variables
+        [myFraction setNumerator:1];
+        [myFraction setDenominator:3];
+        
+        //Display the Fraction
+        NSLog(@"The value of myFraction is:");
+        [myFraction print];
     }
     return 0;
 }
